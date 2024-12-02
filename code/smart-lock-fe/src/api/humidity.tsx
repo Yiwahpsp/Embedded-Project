519cb2456ed41c76fd703f84c2c82dd149cbbf0d
JiratchayaPremsil <156444689+JiratchayaPremsil@users.noreply.github.com>
2024-12-02 11:32:55 +0700

    fix: path

--- code/smart-lock-fe/src/api/humidity.tsx
+++ code/smart-lock-fe/src/api/humidity.tsx
@@ -1,7 +1,5 @@
 export async function getHumid() {
     try {
-        const url = `https://sgp1.blynk.cloud/external/api/get?token=GkfMi5Ps6KQlrQahmVq0Ku_bqRewBTFw&V1`
-        console.log(url)
         const response = await fetch(`https://sgp1.blynk.cloud/external/api/get?token=GkfMi5Ps6KQlrQahmVq0Ku_bqRewBTFw&V1`)
         if (!response) {
             return null;
